62e4429ef26f8189e73b06c74882784d080aa328
Pratyayee-zluri <pratyayee.d@zluri.com>
2025-08-27 15:49:12 +0530

    Boilerplate Code

---
+++ src/main/java/com/spring/memodb/MemodbApplication.java
@@ -0,0 +1,13 @@
+package com.spring.memodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/memodb/MemodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.memodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
